3c47d71024870b8cac5aa132a9929ba77bf380f6
Julie <gervaisdoyon.julie@gmail.com>
2024-11-08 11:29:30 -0500

    refractor code

--- web_scrapping.py
+++ web_scrapping.py
@@ -5,29 +5,6 @@ from bs4 import BeautifulSoup
 import json
 from urllib.parse import urljoin
 
-
-# Tor SOCKS proxy configuration
-proxies = {
-    'http': 'socks5h://127.0.0.1:9050',
-    'https': 'socks5h://127.0.0.1:9050'
-}
-base_url = 'https://reycdxyc24gf7jrnwutzdn3smmweizedy7uojsa7ols6sflwu25ijoyd.onion'
-archive_url = urljoin(base_url, "/archives/")
-
-def get_page(url):
-    return requests.get(url, proxies=proxies, headers={"User-Agent": "Mozilla/5.0"})
-
-response = get_page(archive_url)
-
-if response.status_code == 200:
-    print("Tor connection")
-else:
-    print("No Tor connection")
-
-soup = BeautifulSoup(response.text, 'html.parser')
-
-blog_posts = []
-
 def extract_information(page_to_extract):
     title = page_to_extract.find("h1", class_="posttitle p-name").string.strip()
 
@@ -69,16 +46,34 @@ def extract_from_list(list_page):
 
         extract_information(page_soup)
 
+def get_page(url):
+    return requests.get(url, proxies=proxies, headers={"User-Agent": "Mozilla/5.0"})
+
+# Tor SOCKS proxy configuration
+proxies = {
+    'http': 'socks5h://127.0.0.1:9050',
+    'https': 'socks5h://127.0.0.1:9050'
+}
+base_url = 'https://reycdxyc24gf7jrnwutzdn3smmweizedy7uojsa7ols6sflwu25ijoyd.onion'
+archive_url = urljoin(base_url, "/archives/")
+
+response = get_page(archive_url)
+
+if response.status_code == 200:
+    print("Tor connection")
+else:
+    print("No Tor connection")
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+blog_posts = []
+
 extract_from_list(soup)
 page = next_page(soup)
-nb_pages = 0
 
-while page is not None:
-    extract_from_list(page)
-    page = next_page(page)
-    nb_pages = nb_pages + 1
-    print(nb_pages)
-    print(blog_posts)
+# while page is not None:
+#     extract_from_list(page)
+#     page = next_page(page)
 
 with open("blog_posts.json", "w", encoding="utf-8") as f:
     json.dump(blog_posts, f, ensure_ascii=False, indent=4)
