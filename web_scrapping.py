c0d9ca23d67f0ddc133fdc8253725efeeebbfa60
Julie <gervaisdoyon.julie@gmail.com>
2024-11-08 11:51:59 -0500

    refractor code

--- web_scrapping.py
+++ web_scrapping.py
@@ -1,10 +1,10 @@
 
-
 import requests
 from bs4 import BeautifulSoup
 import json
 from urllib.parse import urljoin
 
+#Fonction pour extraire les informations d'un article de Blog
 def extract_information(page_to_extract):
     title = page_to_extract.find("h1", class_="posttitle p-name").string.strip()
 
@@ -24,6 +24,7 @@ def extract_information(page_to_extract):
         'content': content,
     })
 
+#Fonction pour obtenir la page suivante
 def next_page(page_to_get):
     next_page_href = page_to_get.find("div", class_ = "pagination").find("a").get("href")
     if next_page_href is None:
@@ -33,6 +34,7 @@ def next_page(page_to_get):
     response_page = get_page(next_page_full_url)
     return BeautifulSoup(response_page.content, 'html.parser')
 
+#Fonction pour extraire les URLs des articles et récupérer leur contenu
 def extract_from_list(list_page):
     for post in list_page.find_all("li", class_ = "post-item"):
 
@@ -46,6 +48,7 @@ def extract_from_list(list_page):
 
         extract_information(page_soup)
 
+# Fonction pour obtenir une Page
 def get_page(url):
     return requests.get(url, proxies=proxies, headers={"User-Agent": "Mozilla/5.0"})
 
@@ -57,6 +60,7 @@ proxies = {
 base_url = 'https://reycdxyc24gf7jrnwutzdn3smmweizedy7uojsa7ols6sflwu25ijoyd.onion'
 archive_url = urljoin(base_url, "/archives/")
 
+#Requête initiale et Parsing HTML
 response = get_page(archive_url)
 
 if response.status_code == 200:
@@ -66,8 +70,10 @@ else:
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
+#Initialiser une Liste pour les articles de Blog
 blog_posts = []
 
+#Extraction initiale et boucle de pagination
 extract_from_list(soup)
 page = next_page(soup)
 
@@ -75,6 +81,7 @@ while page is not None:
     extract_from_list(page)
     page = next_page(page)
 
+#Sauvegarde en fichier JSON
 with open("blog_posts.json", "w", encoding="utf-8") as f:
     json.dump(blog_posts, f, ensure_ascii=False, indent=4)
 
